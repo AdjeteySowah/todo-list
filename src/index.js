ce301713ab10915b7458da0ed6cade4b4cffeabe
Emmanuel Adjetey Sowah <adjetey.sowah.321@gmail.com>
2025-02-24 22:03:58 +0000

    Customize template repository

---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+import "./styles.css";
+
+import { logger } from "./modules/controller.js";
+// import { menuTab } from "./modules/menu.js";
+// import { contactTab } from "./modules/contact.js";
+
+logger();
