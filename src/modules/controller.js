fecc03c72f38810e097738ad7cf80c7ecca11651
Emmanuel Adjetey Sowah <adjetey.sowah.321@gmail.com>
2025-02-27 21:18:45 +0000

    Add click a event and a handler to sidebar

--- src/modules/controller.js
+++ src/modules/controller.js
@@ -1,3 +1,15 @@
-export function logger() {
-   console.log("It's working!")
+
+import { setActiveTab, renderTabContent } from "./ui.js";
+
+function handleTabClick(event) {
+   let selectedTab = event.target.closest(".sidebar__item");
+   if (selectedTab) {
+      setActiveTab(selectedTab);
+      renderTabContent(selectedTab);
+   }
+}
+
+export function initTabClickEvent() {
+   let sidebar = document.querySelector(".sidebar");
+   sidebar.addEventListener("click", handleTabClick);
 }
