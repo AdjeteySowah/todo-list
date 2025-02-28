a6cf46be0be61b93d7de2ffc548ae91d8abb072e
Emmanuel Adjetey Sowah <adjetey.sowah.321@gmail.com>
2025-02-28 23:27:34 +0000

    Add click handlers for new dynamic elements

--- src/modules/controller.js
+++ src/modules/controller.js
@@ -1,5 +1,5 @@
 
-import { setActiveTab, renderTabContent } from "./ui.js";
+import { setActiveTab, renderTabContent, renderInboxInput } from "./ui.js";
 
 function handleTabClick(event) {
    let selectedTab = event.target.closest(".sidebar__item");
@@ -12,4 +12,16 @@ function handleTabClick(event) {
 export function initTabClickEvent() {
    let sidebar = document.querySelector(".sidebar");
    sidebar.addEventListener("click", handleTabClick);
+}
+
+function handleInboxAddTaskClick(event) {
+   let selectedAction = event.target.closest(".main-content__action--add-task");
+   if (selectedAction && selectedAction.parentElement.firstElementChild.textContent.trim() === "Inbox") {
+      renderInboxInput();
+   }
+}
+
+export function listenForAddTaskClick() {
+   let mainContent = document.querySelector(".main-content");
+   mainContent.addEventListener("click", handleInboxAddTaskClick);
 }
