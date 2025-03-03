f5600868e4b5d3c11ab0c7d727089862556af710
Emmanuel Adjetey Sowah <adjetey.sowah.321@gmail.com>
2025-03-03 23:46:30 +0000

    Use appropriate and befitting class names

--- src/modules/ui.js
+++ src/modules/ui.js
@@ -1,7 +1,4 @@
 
-import plusIcon from "../assets/images/plus.svg";
-import deleteIcon from "../assets/images/delete.svg";
-
 import { listenForAddTaskClick } from "./controller.js";
 
 export function setActiveTab(selectedTab) {
@@ -32,12 +29,7 @@ export function renderTabContent(selectedTab) {
          let div = document.createElement("div");
       if (selectedTab.firstElementChild.textContent.trim() === "Inbox") {
          div.setAttribute("class", "main-content__action--add-task");
-            let img = document.createElement("img");
-            img.setAttribute("src", plusIcon);
-            img.setAttribute("alt", "plus icon");
-            img.setAttribute("class", "main-content__add-task-img");
             let textNode = document.createTextNode(" Add Task");
-         div.appendChild(img);
          div.appendChild(textNode);
       } else if (
          selectedTab.firstElementChild.textContent.trim() === "Today" || 
@@ -47,22 +39,12 @@ export function renderTabContent(selectedTab) {
          div.setAttribute("class", "main-content__action");
             let div1 = document.createElement("div");
             div1.setAttribute("class", "main-content__action--add-task");
-               let img1 = document.createElement("img");
-               img1.setAttribute("src", plusIcon);
-               img1.setAttribute("alt", "plus icon");
-               img1.setAttribute("class", "main-content__add-task-img");
                let textNode1 = document.createTextNode(" Add Task");
-            div1.appendChild(img1);
             div1.appendChild(textNode1);
 
             let div2 = document.createElement("div");
             div2.setAttribute("class", "main-content__action--delete-project");
-               let img2 = document.createElement("img");
-               img2.setAttribute("src", deleteIcon);
-               img2.setAttribute("alt", "plus icon");
-               img2.setAttribute("class", "main-content__delete-project-img");
                let textNode2 = document.createTextNode(" Delete Project");
-            div2.appendChild(img2);
             div2.appendChild(textNode2);
          div.appendChild(div1);
          div.appendChild(div2);
@@ -76,23 +58,25 @@ export function renderTabContent(selectedTab) {
 
 export function renderInboxInput() {
    let mainContent = document.querySelector(".main-content");
-   let lastChild = document.querySelector(".main-content__action--add-task");
-   mainContent.removeChild(lastChild);
+   let mainContLastChild = document.querySelector(".main-content__action--add-task");
+   mainContent.removeChild(mainContLastChild);
       let div = document.createElement("div");
+      div.setAttribute("class", "main-content__input-action");
          let input = document.createElement("input");
          input.setAttribute("type", "text");
          input.setAttribute("maxlength", "50");
-         input.setAttribute("class", "proj-input");
+         input.setAttribute("class", "main-content__task-input");
          let div1 = document.createElement("div");
-         div1.setAttribute("class", "main-content__action")
+         div1.setAttribute("class", "main-content__new-action")
             let div11 = document.createElement("div");
+            div11.setAttribute("class", "main-content__new-add");
             div11.textContent = "Add";
             let div12 = document.createElement("div");
+            div12.setAttribute("class", "main-content__new-cancel");
             div12.textContent = "Cancel";
          div1.appendChild(div11);
          div1.appendChild(div12);
       div.appendChild(input);
       div.appendChild(div1);
    mainContent.appendChild(div);
-
 }
