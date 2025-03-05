6927350010e9177f939f89bb3c639f01e22cdedd
Emmanuel Adjetey Sowah <adjetey.sowah.321@gmail.com>
2025-03-05 12:25:24 +0000

    Import right renamed function

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 
 import "./styles.css";
 
-import { initTabClickEvent, listenForAddTaskClick } from "./modules/controller.js";
+import { initTabClickEvent, listenForClicksInMain } from "./modules/controller.js";
 import { renderTabContent } from "./modules/ui.js";
 
 function init() {
    initTabClickEvent();
    renderTabContent();
-   listenForAddTaskClick();
+   listenForClicksInMain();
 }
 
 init();
