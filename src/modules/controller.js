2b5501aecf4e115f729aebc56da925b22943cd41
Emmanuel Adjetey Sowah <adjetey.sowah.321@gmail.com>
2025-03-05 12:32:48 +0000

    Create click handlers for task adding, deleting etc

--- src/modules/controller.js
+++ src/modules/controller.js
@@ -1,11 +1,12 @@
 
-import { setActiveTab, renderTabContent, renderInboxInput } from "./ui.js";
+import { setActiveTab, renderTabContent, renderInboxInput, fadeAndStrikeThroughTask } from "./ui.js";
+import { addTaskToArray, removeTaskFromArray, updateTaskStatus } from "./taskManager.js";
 
 function handleTabClick(event) {
    let selectedTab = event.target.closest(".sidebar__item");
    if (selectedTab) {
       setActiveTab(selectedTab);
-      renderTabContent(selectedTab);
+      renderTabContent();
    }
 }
 
@@ -15,13 +16,44 @@ export function initTabClickEvent() {
 }
 
 function handleInboxAddTaskClick(event) {
-   let selectedAction = event.target.closest(".main-content__action--add-task");
-   if (selectedAction && selectedAction.parentElement.firstElementChild.textContent.trim() === "Inbox") {
+   let selectedElement = event.target.closest(".main-content__action--add-task");
+   if (selectedElement && selectedElement.parentElement.firstElementChild.textContent.trim() === "Inbox") {
       renderInboxInput();
    }
 }
 
-export function listenForAddTaskClick() {
+function handleAddTaskClick(event) {
+   let selectedElement = event.target.classList.contains("main-content__new-add");
+   if (selectedElement) {
+      addTaskToArray();
+      renderTabContent();
+   }
+}
+
+function handleRemoveTaskClick(event) {
+   let selectedElement = event.target.classList.contains("main-content__task-delete");
+   if (selectedElement) {
+      removeTaskFromArray(event);
+      renderTabContent();
+   }
+}
+
+function handleTaskCompletionStatusClick(event) {
+   let selectedElement = event.target.classList.contains("main-content__checkbox");
+   if (selectedElement) {
+      fadeAndStrikeThroughTask(event);
+      updateTaskStatus(event);
+   }
+}
+
+function handleClicksInMain(event) {
+   handleInboxAddTaskClick(event);
+   handleAddTaskClick(event);
+   handleRemoveTaskClick(event);
+   handleTaskCompletionStatusClick(event);
+}
+
+export function listenForClicksInMain() {
    let mainContent = document.querySelector(".main-content");
-   mainContent.addEventListener("click", handleInboxAddTaskClick);
+   mainContent.addEventListener("click", handleClicksInMain);
 }
