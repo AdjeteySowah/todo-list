21285184c64201b5b19730b6c95a4210a12115b4
Emmanuel Adjetey Sowah <adjetey.sowah.321@gmail.com>
2025-03-07 16:13:31 +0000

    Retrieve task input value from main content

--- src/modules/taskManager.js
+++ src/modules/taskManager.js
@@ -4,7 +4,8 @@ import { createTask } from "./task.js";
 export let tasks = [];
 
 export function addTaskToArray() {
-   let taskInput = document.querySelector(".main-content__task-input");
+   let mainContent = document.querySelector(".main-content");
+   let taskInput = mainContent.querySelector(".input");
    let newTask = createTask(taskInput.value);
    tasks.push(newTask);
 }
