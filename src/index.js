af1722a4109e6318775cad5f1279b5545f42dd1a
Emmanuel Adjetey Sowah <adjetey.sowah.321@gmail.com>
2025-03-09 16:51:25 +0000

    Add right spaces/indentation

--- src/index.js
+++ src/index.js
@@ -5,18 +5,9 @@ import { listenForClicksInSidebar, listenForClicksInMain } from "./modules/contr
 import { renderTabContent } from "./modules/ui.js";
 
 function init() {
-   listenForClicksInSidebar();
-   renderTabContent();
-   listenForClicksInMain();
+  listenForClicksInSidebar();
+  renderTabContent();
+  listenForClicksInMain();
 }
 
 init();
-
-
-let projectContainer = document.querySelector(".sidebar__projects-container");
-
-let observer = new MutationObserver(() => {
-  projectContainer.scrollTop = projectContainer.scrollHeight;
-});
-
-observer.observe(projectContainer, { childList: true });
