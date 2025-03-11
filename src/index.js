cf9c93ce5adfd80c0ee9d8778b0665395ed4032f
Emmanuel Adjetey Sowah <adjetey.sowah.321@gmail.com>
2025-03-11 19:33:21 +0000

    Listen for clicks in the dialog

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 
 import "./styles.css";
 
-import { listenForClicksInSidebar, listenForClicksInMain } from "./modules/controller.js";
+import { listenForClicksInSidebar, listenForClicksInMain, listenForClicksInModal } from "./modules/controller.js";
 import { renderTabContent } from "./modules/ui.js";
 
 function init() {
   listenForClicksInSidebar();
   renderTabContent();
   listenForClicksInMain();
+  listenForClicksInModal();
 }
 
 init();
