364cc41ce189c1cd2e542addd08768715aab4e5a
Emmanuel Adjetey Sowah <adjetey.sowah.321@gmail.com>
2025-03-14 23:27:12 +0000

    Refactor code so that tasks in projects, not just in inbox can also de modified or deleted

--- src/modules/taskManager.js
+++ src/modules/taskManager.js
@@ -1,10 +1,10 @@
 
 import { createTask } from "./task.js";
 import { projects } from "./project.js";
+import { taskEditClickEventTarget } from "./controller.js";
 
 export let inboxTasks = [];
 
-   // create conditions for adding, removing, and maybe update tasks in inbox or in projects
 export function addTaskToArray() {
    let activeTab = document.querySelector(".active");
 
@@ -34,32 +34,56 @@ export function addTaskToArray() {
    // completed? / not completed?
 export function updateTaskStatus(event) {
    let taskIndex = event.target.closest(".main-content__task").getAttribute("data-index");
-   inboxTasks[taskIndex].changeCompletedStatus();
+
+   if (event.target.closest(".main-content").firstElementChild.textContent === "Inbox") {
+      inboxTasks[taskIndex].changeCompletedStatus();
+   } else {
+      for (let key in projects) {
+         if (key === event.target.closest(".main-content").firstElementChild.textContent) {
+            projects[key][taskIndex].changeCompletedStatus();
+         }
+      }
+   }
 }
 
    // shows modal before editing can be done
-export function updateTaskDetails(selectedElement) {
+export function updateTaskDetails() {
    let activeTab = document.querySelector(".active");
-   let taskIndex = selectedElement;       //.getAttribute("data-index");
-   console.log(taskIndex);
+   let taskIndex = taskEditClickEventTarget.closest(".main-content__task").getAttribute("data-index");
 
-   // let titleInput = document.querySelector("input[type='text']");
-   // let descTextarea = document.querySelector("textarea");
-   // let dateInput = document.querySelector("input[type='date']");
-   // let prioSelect = document.querySelector("select");
-   // let selectedOption = prioSelect.options[prioSelect.selectedIndex];
+   let titleInput = document.querySelector("input[type='text']");
+   let descTextarea = document.querySelector("textarea");
+   let dateInput = document.querySelector("input[type='date']");
+   let prioSelect = document.querySelector("select");
+   let selectedOption = prioSelect.options[prioSelect.selectedIndex];
 
-   // if (activeTab.firstElementChild.textContent.trim() === "Inbox") {
-   //    inboxTasks[taskIndex].changeTitle(titleInput.value);
-   //    inboxTasks[taskIndex].changeDescription(descTextarea.value);
-   //    inboxTasks[taskIndex].changeDueDate(dateInput.value);
-   //    inboxTasks[taskIndex].changePriority(selectedOption.text);
-   // }
-
-   // retrieve task details from related array, fill the form with the data, and allow users to save new edits
+   if (activeTab.firstElementChild.textContent.trim() === "Inbox") {
+      inboxTasks[taskIndex].changeTitle(titleInput.value);
+      inboxTasks[taskIndex].changeDescription(descTextarea.value);
+      inboxTasks[taskIndex].changeDueDate(dateInput.value);
+      inboxTasks[taskIndex].changePriority(selectedOption.text);
+   } else {
+      for (let key in projects) {
+         if (key === activeTab.firstElementChild.textContent.trim()) {
+            projects[key][taskIndex].changeTitle(titleInput.value);
+            projects[key][taskIndex].changeDescription(descTextarea.value);
+            projects[key][taskIndex].changeDueDate(dateInput.value);
+            projects[key][taskIndex].changePriority(selectedOption.text);
+         }
+      }
+   }
 }
 
 export function removeTaskFromArray(event) {
    let taskIndex = event.target.closest(".main-content__task").getAttribute("data-index");
-   inboxTasks.splice(taskIndex, 1);
+
+   if (event.target.closest(".main-content").firstElementChild.textContent === "Inbox") {
+      inboxTasks.splice(taskIndex, 1);
+   } else {
+      for (let key in projects) {
+         if (key === event.target.closest(".main-content").firstElementChild.textContent) {
+            projects[key].splice(taskIndex, 1);
+         }
+      }
+   }
 }
