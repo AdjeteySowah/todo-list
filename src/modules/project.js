d36390cccee7db9a2fff4977e9adc1999537f528
Emmanuel Adjetey Sowah <adjetey.sowah.321@gmail.com>
2025-03-15 22:16:46 +0000

    Delete project's array from projects object

--- src/modules/project.js
+++ src/modules/project.js
@@ -1,5 +1,7 @@
 
-let projects = {
+import { renderProjectSection } from "./ui";
+
+export let projects = {
    "Gym": [],
    "Coding": [],
 };
@@ -7,7 +9,18 @@ let projects = {
 export function createProject() {
    let sidebar = document.querySelector(".sidebar");
    let taskInputValue = sidebar.querySelector(".input").value.trim();
-   let formattedTaskInputValue = `${taskInputValue[0].toUpperCase()}${taskInputValue.slice(1)}`
+   let formattedTaskInputValue = `${taskInputValue[0].toUpperCase()}${taskInputValue.slice(1)}`;
    projects[formattedTaskInputValue] = [];
-   console.log(projects);
+   // console.log(projects);
+}
+
+export function deleteProject(selectedElement) {
+   let projectName = selectedElement.closest(".main-content").firstElementChild.textContent;
+   for (let key in projects) {
+      if (key === selectedElement.closest(".main-content").firstElementChild.textContent) {
+         delete projects[key];
+      }
+   }
+   renderProjectSection(projectName);
+   // console.log(projects);
 }
