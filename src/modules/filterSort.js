f15fa172863cd404947a3e30ebaca83a0c6a5bb8
Emmanuel Adjetey Sowah <adjetey.sowah.321@gmail.com>
2025-03-18 23:35:51 +0000

    Get all tasks to be accomplished "today"

--- src/modules/filterSort.js
+++ src/modules/filterSort.js
@@ -1,6 +1,7 @@
 
 import { projects } from "./project.js";
 import { inboxTasks } from "./taskManager.js";
+import { format } from "date-fns";
 
 let allTasks = [];
 
@@ -10,4 +11,23 @@ export function collectAllTasks() {
    for (let key in projects) {
       allTasks.push(...projects[key]);
    }
+
+   console.log(allTasks);
+   getTasksWithDate();
+   getTaskForToday();
 }
+
+let tasksWithDate;
+
+function getTasksWithDate() {
+   tasksWithDate = allTasks.filter(task => task.date !== undefined && task.date !== "");
+}
+
+export let tasksForToday = [];
+
+function getTaskForToday() {
+   tasksForToday = tasksWithDate.filter(task => task.date === format(new Date().toDateString(), "EEE, MMM d, yyyy"));
+}
+
+
+// Find out how tasks can be grouped under "this week" tab
