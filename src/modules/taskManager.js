a7a20f93040d586637fbbee21d4005c8c7e9a2b2
Emmanuel Adjetey Sowah <adjetey.sowah.321@gmail.com>
2025-04-02 23:39:00 +0000

    Ensure that tasks can be deleted, modified etc and reflect anywhere necessary

--- src/modules/taskManager.js
+++ src/modules/taskManager.js
@@ -2,7 +2,8 @@
 import { createTask } from "./task.js";
 import { projects } from "./project.js";
 import { taskEditClickEventTarget } from "./controller.js";
-import { collectAllTasks } from "./filterSort.js";
+import { collectAllTasks, tasksForToday, tasksForTheWeek } from "./filterSort.js";
+import { formatDate } from "./utility.js";
 
 
 export let inboxTasks = [];
@@ -13,20 +14,19 @@ export function addTaskToArray() {
    if (activeTab.firstElementChild.textContent.trim() === "Inbox") {
       let mainContent = document.querySelector(".main-content");
       let taskInput = mainContent.querySelector(".input");
-      let newTask = createTask(taskInput.value);
+      let newTask = createTask(taskInput.value, undefined, undefined, undefined, "Inbox");
       inboxTasks.push(newTask);
-   } else if (
-      activeTab.firstElementChild.textContent.trim() !== "Today" || 
-      activeTab.firstElementChild.textContent.trim() !== "This Week"
-      ) {
+   } else if (activeTab.firstElementChild.textContent.trim() !== "Today" || 
+      activeTab.firstElementChild.textContent.trim() !== "This week") {
       let titleInput = document.querySelector("input[type='text']");
       let descTextarea = document.querySelector("textarea");
       let dateInput = document.querySelector("input[type='date']");
       let prioSelect = document.querySelector("select");
       let selectedOption = prioSelect.options[prioSelect.selectedIndex];
-      let newTask = createTask(titleInput.value, descTextarea.value, dateInput.value, selectedOption.text);
+
       for (let key in projects) {
          if (key === activeTab.firstElementChild.textContent.trim()) {
+            let newTask = createTask(titleInput.value, descTextarea.value, formatDate(dateInput.value), selectedOption.text, key);
             projects[key].push(newTask);
          }
       }
@@ -41,7 +41,12 @@ export function updateTaskStatus(event) {
 
    if (event.target.closest(".main-content").firstElementChild.textContent === "Inbox") {
       inboxTasks[taskIndex].changeCompletedStatus();
-   } else {
+   } else if (event.target.closest(".main-content").firstElementChild.textContent === "Today") {
+      tasksForToday[taskIndex].changeCompletedStatus();
+   } else if (event.target.closest(".main-content").firstElementChild.textContent === "This week") {
+      tasksForTheWeek[taskIndex].changeCompletedStatus();
+   }
+   else {
       for (let key in projects) {
          if (key === event.target.closest(".main-content").firstElementChild.textContent) {
             projects[key][taskIndex].changeCompletedStatus();
@@ -66,6 +71,16 @@ export function updateTaskDetails() {
       inboxTasks[taskIndex].changeDescription(descTextarea.value);
       inboxTasks[taskIndex].changeDueDate(dateInput.value);
       inboxTasks[taskIndex].changePriority(selectedOption.text);
+   } else if (activeTab.firstElementChild.textContent.trim() === "Today") {
+      tasksForToday[taskIndex].changeTitle(titleInput.value);
+      tasksForToday[taskIndex].changeDescription(descTextarea.value);
+      tasksForToday[taskIndex].changeDueDate(dateInput.value);
+      tasksForToday[taskIndex].changePriority(selectedOption.text);
+   } else if (activeTab.firstElementChild.textContent.trim() === "This week") {
+      tasksForTheWeek[taskIndex].changeTitle(titleInput.value);
+      tasksForTheWeek[taskIndex].changeDescription(descTextarea.value);
+      tasksForTheWeek[taskIndex].changeDueDate(dateInput.value);
+      tasksForTheWeek[taskIndex].changePriority(selectedOption.text);
    } else {
       for (let key in projects) {
          if (key === activeTab.firstElementChild.textContent.trim()) {
@@ -76,6 +91,8 @@ export function updateTaskDetails() {
          }
       }
    }
+
+   collectAllTasks();
 }
 
 export function removeTaskFromArray(event) {
@@ -83,6 +100,30 @@ export function removeTaskFromArray(event) {
 
    if (event.target.closest(".main-content").firstElementChild.textContent === "Inbox") {
       inboxTasks.splice(taskIndex, 1);
+   } else if (event.target.closest(".main-content").firstElementChild.textContent === "Today") {
+      let deletedTask = tasksForToday.splice(taskIndex, 1);
+
+      if (deletedTask[0].project === "Inbox") {
+         inboxTasks.splice(taskIndex, 1);
+      } else {
+         for (let key in projects) {
+            if (key === deletedTask[0].project) {
+               projects[key].splice(taskIndex, 1);
+            }
+         }
+      }
+   } else if (event.target.closest(".main-content").firstElementChild.textContent === "This week") {
+      let deletedTask = tasksForTheWeek.splice(taskIndex, 1);
+
+      if (deletedTask[0].project === "Inbox") {
+         inboxTasks.splice(taskIndex, 1);
+      } else {
+         for (let key in projects) {
+            if (key === deletedTask[0].project) {
+               projects[key].splice(taskIndex, 1);
+            }
+         }
+      }
    } else {
       for (let key in projects) {
          if (key === event.target.closest(".main-content").firstElementChild.textContent) {
@@ -90,4 +131,6 @@ export function removeTaskFromArray(event) {
          }
       }
    }
+
+   collectAllTasks();
 }
