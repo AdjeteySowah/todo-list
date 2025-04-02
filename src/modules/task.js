9b92b3c2cc1cc1b531b882a425be7dbe112e9daa
Emmanuel Adjetey Sowah <adjetey.sowah.321@gmail.com>
2025-04-02 23:40:54 +0000

    Add a 'project' property to tasks objects

--- src/modules/task.js
+++ src/modules/task.js
@@ -1,11 +1,12 @@
 import { formatDate } from "./utility.js";
 
-export function createTask(title, description, date, priority) {
+export function createTask(title, description = undefined, date = undefined, priority = undefined, project) {
    return {
       title,
       description,
       date,
       priority,
+      project,
       completed: false,
       
       changeTitle(title) {
